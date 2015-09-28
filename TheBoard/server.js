6db320d92a5ccaf7b65d2ca18d725af8d7d9abc7
Bruno Quintella <quintelab@gmail.com>
2015-09-28 14:46:03 -0300

    Implementation using EJS

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -2,13 +2,16 @@
 var port = process.env.port || 1337
 var express = require('express');
 var app = express();
+var ejsEngine = require('ejs-locals');
 
 //Setup the View Engine
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade'); // jade view engine
+app.engine('ejs', ejsEngine); //support master pages
+app.set('view engine', 'ejs'); // ejs view engine
 
 app.get('/', function (req, res) {
-	//res.send('<html><body><h1>Express</h1></body></html>');
-	res.render('jade/index', { title: 'Express + Jade' });
+	//res.render('jade/index', { title: 'Express + Jade' });
+    res.render('ejs/index', { title: 'Express + EJS' });
 });
 
 app.get('/api/users', function (req, res) {
