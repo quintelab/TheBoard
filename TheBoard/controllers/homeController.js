abe53820808d21cf78027e9a7ba1ee86b0bbc5fe
Bruno Quintella <quintelab@gmail.com>
2015-09-30 08:14:51 -0300

    Adding controllers

---
+++ TheBoard/controllers/homeController.js
@@ -0,0 +1,7 @@
+﻿(function (homeController) {
+    homeController.init = function (app) {
+        app.get('/', function (req, res) {
+            res.render('index', { title: 'Express + Vash' });
+        });
+    };
+})(module.exports);
