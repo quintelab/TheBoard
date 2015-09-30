4fa7abf8d7b6ea55b49b72c5052270d81d1d65ec
Bruno Quintella <quintelab@gmail.com>
2015-09-30 15:17:28 -0300

    Implementing grunt

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,7 +6,7 @@
         app.get('/', function (req, res) {
             
             data.getNoteCategories(function (err, results) {
-                res.render('index', { title: 'Express + Vash', error: err, categories: results });
+                res.render('index', { title: 'The Board', error: err, categories: results });
             });
         });
     };
---
+++ gruntfile.js
@@ -0,0 +1,13 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        nodemon: {
+            script: 'server.js',
+            options: {
+                watchedExtensions: ['js']
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.registerTask('default', ['nodemon']);
+};
