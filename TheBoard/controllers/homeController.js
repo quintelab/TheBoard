334cd62c6bd91dd1d9f1a8cfc8011fc384fbf934
Bruno Quintella <quintelab@gmail.com>
2015-09-30 09:16:31 -0300

    Adding a service folder to provide data

--- TheBoard/controllers/homeController.js
+++ TheBoard/controllers/homeController.js
@@ -1,7 +1,13 @@
 ﻿(function (homeController) {
+    
+    var data = require('../data');
+
     homeController.init = function (app) {
         app.get('/', function (req, res) {
-            res.render('index', { title: 'Express + Vash' });
+            
+            data.getNoteCategories(function (err, results) {
+                res.render('index', { title: 'Express + Vash', error: err, categories: results });
+            });
         });
     };
 })(module.exports);
