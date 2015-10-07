243459348c8d82cddb6fd6e03c1d238919505618
quintelab <quintelab@gmail.com>
2015-10-07 11:54:45 -0300

    Saving new notes in the database

--- public/js/notesViews.js
+++ public/js/notesViews.js
@@ -5,10 +5,7 @@
   theModule.controller('notesViewController', ['$scope', "$window", "$http",
     function($scope, $window, $http){
       $scope.notes = [];
-      $scope.newNote = {
-        note: '',
-        color: 'yellow'
-      };
+      $scope.newNote = createBlankNote();
 
       //Get the category name
       var urlParts = $window.location.pathname.split('/');
@@ -23,7 +20,26 @@
           // Error
           alert(err);
         });
+
+        $scope.save = function () {
+          $http.post(notesUrl, $scope.newNote)
+            .then(function (result) {
+              //success
+              $scope.notes.push(result.data);
+              $scope.newNote = createBlankNote();
+            }, function (err) {
+              // failure
+              // TODO
+            });
+        };
     }
   ]);
+
+  function createBlankNote() {
+    return {
+      note: '',
+      color: 'yellow'
+    };
+  }
 
 })(window.angular);
